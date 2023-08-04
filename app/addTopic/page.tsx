0ffec862efe4ea1990230ff4d686526184f7d516
Abhishek Bagiyal <63339447+bagiyal@users.noreply.github.com>
2023-08-04 23:59:15 +0530

    Add Topics

--- app/addTopic/page.tsx
+++ app/addTopic/page.tsx
@@ -1,9 +1,49 @@
+"use client"
 
+import { useState } from "react";
 
-export default function AddTopic(){
-    return <form style={{display:'flex',flexDirection:'column'}}>
-        <input style={{display:'flex',borderWidth:4,padding:5,borderColor:'slate-500'}} type="text" placeholder="Topic Title" />
-        <input style={{display:'flex',borderWidth:4,padding:5,borderColor:'slate-500'}} type="text" placeholder="Topic Description" />
-        <button style={{backgroundColor:'green',fontWeight:'bolder'}}>Topic</button>
+export default function AddTopic() {
+  const [title,setTitle] = useState("");
+  const [description,setDescription] = useState("");
+  const handleChange = () => {
+    console.log(title);
+    console.log(description);
+    if(!title || !description) {
+        alert("Please select a title");
+    }
+  };
+    return (
+    <form style={{ display: "flex", flexDirection: "column" }} onSubmit={handleChange}>
+      <input
+        style={{
+          display: "flex",
+          borderWidth: 4,
+          padding: 5,
+          borderColor: "slate-500",
+        }}
+        type="text"
+        placeholder="Topic Title"
+        onChange={(value:any) => {
+            console.log(value);
+            setTitle(value.target.value);
+        }}
+        value={title}
+      />
+      <input
+        style={{
+          display: "flex",
+          borderWidth: 4,
+          padding: 5,
+          borderColor: "slate-500",
+        }}
+        type="text"
+        placeholder="Topic Description"
+        onChange={setDescription}
+        value={description}
+      />
+      <button type="submit" style={{ backgroundColor: "green", fontWeight: "bolder" }}>
+        Topic
+      </button>
     </form>
+  );
 }
--- component/TopicList.tsx
+++ component/TopicList.tsx
@@ -2,19 +2,64 @@ import Link from "next/link";
 import RemoveBtn from "./RemoveBtn";
 import { HiPencilAlt } from "react-icons/hi";
 
-export default function TopicList(){
-    return (
-        <div style={{borderWidth:5,borderColor:'black',margin:3,justifyContent:'space-between',backgroundColor:"white",display:'flex',padding:20,}}>
-            <div>
-                <h2 style={{fontWeight:'bolder',fontSize:20}}>Topic Title</h2>
-                <div>Topic Description</div>
-            </div>
-            <div>
-                <RemoveBtn />
-                <Link href={"/editTopic"}>
-                <HiPencilAlt size={24} />
-                </Link>
-            </div>
+const getTopics = async () => {
+  try {
+    const res = await fetch("http://localhost:3000/api/topics", {
+      cache: "no-store",
+    });
+    if (!res.ok) {
+      throw new Error("Failed to fetch topics");
+    }
+    // console.log("--- res --",res);
+    return res.json();
+  } catch (error) {
+    console.log("Error Loading Error", error);
+  }
+};
+const delTopics = async (id) => {
+  try{
+    const res = await fetch(`http://localhost:3000/api/topics/${id}`, {
+      method: "DELETE",
+      cache: "no-store",
+    });
+    if (!res.ok) {
+      throw new Error("Failed to delete topics");
+    }
+    // console.log("--- res --",res);
+    return res.json();
+  }catch (error) {
+   console.log("Error Loading Error", error);
+  };
+}
+export default async function TopicList() {
+  const data  = await getTopics();
+  console.log("=>+>+>",data);
+  return (
+    <>
+      {data.map((topics:any) => (
+        <div
+          style={{
+            borderWidth: 5,
+            borderColor: "black",
+            margin: 3,
+            justifyContent: "space-between",
+            backgroundColor: "white",
+            display: "flex",
+            padding: 20,
+          }}
+        >
+          <div>
+            <h2 style={{ fontWeight: "bolder", fontSize: 20 }}>{topics.title}</h2>
+            <div>{topics.description}</div>
+          </div>
+          <div>
+            <RemoveBtn />
+            <Link href={`/editTopic/${topics._id}`}>
+              <HiPencilAlt size={24} />
+            </Link>
+          </div>
         </div>
-    );
+      ))}
+    </>
+  );
 }
